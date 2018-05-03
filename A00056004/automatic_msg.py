472a7f4eaa545dbc31621b6a28dbe80885faf53a
Brayan Henao <bryanhenao96@gmail.com>
2018-05-02 20:53:56 -0500

    Update python file

--- A00056004/automatic_msg.py
+++ A00056004/automatic_msg.py
@@ -10,14 +10,14 @@ from slackclient import SlackClient
 
 
 @cli.app.CommandLineApp
-def automatic_message():
+def automatic_message(app):
     token = 'xoxp-358505374103-356911436881-357460471010-5b1cd8ac0d38e205656a76269314c67a'
     client = SlackClient(token)
-    cpu_usage = psu.cpu_times()
-    cpu_percent = psu.cpu_percent(interval=1, percpu=True)
-    disk_usage = psu.disk_usage('/')
-    memory_virtual = psu.virtual_memory()
-    memory_swap = psu.swap_memory()
+    cpu_usage = str(psu.cpu_times())
+    cpu_percent = str(psu.cpu_percent(interval=1, percpu=True))
+    disk_usage = str(psu.disk_usage('/'))
+    memory_virtual = str(psu.virtual_memory())
+    memory_swap = str(psu.swap_memory())
 
     cpu = 'CPU INFO: \n' + cpu_usage + '\n' + cpu_percent + '\n\n'
     disk = 'DISK INFO: \n' + disk_usage + '\n\n'
